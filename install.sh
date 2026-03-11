f9aeb7f58b55074858c99d90017aa210b92b2969
Ryhnfii01 <rayhans01@gmail.com>
2026-03-12 02:37:03 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,11 @@ REPO="Ryhnfii01/RobloxAPK"
 TAG="APK"
 
 echo ""
+echo "Membersihkan file lama..."
+
+rm -f *.apk
+
+echo ""
 echo "Mencari semua APK di GitHub Release..."
 
 APK_URLS=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG \
@@ -20,16 +25,14 @@ APK_URLS=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG \
 | cut -d '"' -f 4)
 
 echo ""
-echo "Download APK..."
+echo "Downloading APK..."
 
 for url in $APK_URLS
 do
     file=$(basename "$url")
     echo "Downloading $file"
-    wget -q --show-progress "$url" &
+    wget -q --show-progress "$url"
 done
-
-wait
 
 echo ""
 echo "Installing APK..."
