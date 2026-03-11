6ec4ce67579aabd8095cb9294cfdd1683d4b37e4
Ryhnfii01 <rayhans01@gmail.com>
2026-03-12 03:19:36 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -11,18 +11,25 @@ pkg install curl wget -y >/dev/null 2>&1
 REPO="Ryhnfii01/RobloxAPK"
 TAG="APK"
 
-SD_DIR="/sdcard/Download/APK"
-TMP_DIR="/data/local/tmp"
+DIR="/sdcard/Download/APK"
 
-mkdir -p "$SD_DIR"
+echo ""
+echo "Menyiapkan folder..."
+
+mkdir -p "$DIR"
 
 echo "Menghapus APK lama..."
-rm -f "$SD_DIR"/*.apk
+rm -f "$DIR"/*.apk
 
 echo ""
 echo "Mencari semua APK di GitHub Release..."
 
 APK_URLS=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | grep browser_download_url | grep ".apk" | cut -d '"' -f 4)
+
+if [ -z "$APK_URLS" ]; then
+    echo "Tidak ada APK ditemukan di release."
+    exit 1
+fi
 
 echo ""
 echo "Downloading APK..."
@@ -30,21 +37,19 @@ echo "Downloading APK..."
 for url in $APK_URLS; do
     file=$(basename "$url")
     echo "Downloading $file"
-    wget --show-progress -O "$SD_DIR/$file" "$url"
+    wget --show-progress -O "$DIR/$file" "$url"
 done
 
 echo ""
 echo "Installing APK..."
 
-for apk in "$SD_DIR"/*.apk; do
+for apk in "$DIR"/*.apk; do
     if [ -f "$apk" ]; then
         name=$(basename "$apk")
-
-        cp "$apk" "$TMP_DIR/$name"
-        chmod 644 "$TMP_DIR/$name"
+        size=$(stat -c%s "$apk")
 
         echo "Install $name"
-        cmd package install -r -d "$TMP_DIR/$name"
+        cat "$apk" | pm install -r -d -S "$size"
     fi
 done
 
